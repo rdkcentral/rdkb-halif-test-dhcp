c6a4817d01327f440bb5f77669486a7b63803886
Prasad AO <Prasad_AO@comcast.com>
2023-12-20 11:45:21 +0000

    Review comments addressed

---
+++ skeletons/src/dhcp4cApi.c
@@ -0,0 +1,199 @@
+#include <string.h>
+#include <stdlib.h>
+#include <setjmp.h>
+#include "dhcp4cApi.h"
+
+
+int dhcp4c_get_ert_lease_time(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ert_remain_lease_time(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ert_remain_renew_time(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ert_remain_rebind_time(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ert_config_attempts(int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ert_ifname(char* pName)
+{
+  /*TODO: Implement Me!*/
+  (void)pName;
+  return (int)0;
+}
+
+int dhcp4c_get_ert_fsm_state(int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ert_ip_addr(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ert_mask(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ert_gw(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ert_dns_svrs(ipv4AddrList_t* pList)
+{
+  /*TODO: Implement Me!*/
+  (void)pList;
+  return (int)0;
+}
+
+int dhcp4c_get_ert_dhcp_svr(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ecm_lease_time(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ecm_remain_lease_time(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ecm_remain_renew_time(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ecm_remain_rebind_time(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ecm_config_attempts(int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ecm_ifname(char* pName)
+{
+  /*TODO: Implement Me!*/
+  (void)pName;
+  return (int)0;
+}
+
+int dhcp4c_get_ecm_fsm_state(int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ecm_ip_addr(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ecm_mask(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ecm_gw(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_ecm_dns_svrs(ipv4AddrList_t* pList)
+{
+  /*TODO: Implement Me!*/
+  (void)pList;
+  return (int)0;
+}
+
+int dhcp4c_get_ecm_dhcp_svr(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_emta_remain_lease_time(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_emta_remain_renew_time(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+int dhcp4c_get_emta_remain_rebind_time(unsigned int* pValue)
+{
+  /*TODO: Implement Me!*/
+  (void)pValue;
+  return (int)0;
+}
+
+
+
+
+
